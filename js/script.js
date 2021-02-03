f2d020382344de826df7931be39b3b8b9563c546
Maciej Burak <maciej.burak97@onet.pl>
2021-02-03 11:57:27 +0100

    Add results using if statement

--- js/script.js
+++ js/script.js
@@ -33,3 +33,25 @@ if(randomNumber == 1){
 printMessage('Mój ruch to: ' + computerMove);
 
 
+if( computerMove == 'kamień' && playerMove == 'papier'){
+  printMessage('Ty wygrywasz!');
+} else if( computerMove == 'papier' && playerMove == 'kamień'){
+  printMessage('Ja wygrywam!');
+} else if( computerMove == 'nożyce' && playerMove == 'papier'){
+  printMessage('Ja wygrywam!');
+} else if( computerMove == 'papier' && playerMove == 'nożyce'){
+  printMessage('Ty wygrywasz!');
+} else if( computerMove == 'nożyce' && playerMove == 'kamień'){
+  printMessage('Ty wygrywasz!');
+} else if( computerMove == 'kamień' && playerMove == 'nożyce'){
+  printMessage('Ja wygrywam!');
+} else if( computerMove == 'kamień' && playerMove == 'kamień'){
+  printMessage('Remis!');
+} else if( computerMove == 'papier' && playerMove == 'papier'){
+  printMessage('Remis!');
+} else if( computerMove == 'nożyce' && playerMove == 'nożyce'){
+  printMessage('Remis!');
+} else{
+  printMessage('Nieprawidłowa wartość podana przez użytkownika');
+}
+
