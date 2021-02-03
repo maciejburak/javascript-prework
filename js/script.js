cc24dd3f41c28e15541b1e508c7027fa5e3d5c0f
Maciej Burak <maciej.burak97@onet.pl>
2021-02-03 11:33:48 +0100

    Add firsts if statements

--- js/script.js
+++ js/script.js
@@ -1,3 +1,35 @@
-let playerMove = `papier`;
+let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
 
-printMessage('Zagrałem ' + playerMove + '! Jeśli Twój ruch to nożyce, to wygrywasz!');
+console.log('Gracz wpisał: ' + playerInput);
+
+let playerMove = 'nieznany ruch';
+
+if(playerInput == '1'){
+  	playerMove = 'kamień';
+}else if(playerInput == 2){
+   	playerMove = 'papier';
+}else if(playerInput == 3){
+   	playerMove = 'nożyce';
+}else{
+	playerMove = 'nieznany ruch';
+}
+
+printMessage('Twój ruch to: ' + playerMove);
+
+let randomNumber = Math.floor(Math.random() * 3 + 1);
+
+console.log('Wylosowana liczba to: ' + randomNumber);
+
+let computerMove = 'nieznany ruch';
+
+if(randomNumber == 1){
+   computerMove = 'kamień';
+}else if(randomNumber == 2){
+   computerMove = 'papier';
+}else{
+   computerMove = 'nożyce';
+}
+
+printMessage('Mój ruch to: ' + computerMove);
+
+
