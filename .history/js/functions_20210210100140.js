07dab6094b01cfd20afd39f6772ab0e274c60e97
Maciej Burak <maciej.burak97@onet.pl>
2021-02-10 18:13:01 +0100

    Pulling out functions from playGame and reduce empty lines

--- .history/js/functions_20210210100140.js
+++
@@ -1,9 +0,0 @@
-const printMessage = function (msg) {
-	let div = document.createElement('div');
-	div.innerHTML = msg;
-	document.getElementById('messages').appendChild(div);
-}
-
-const clearMessages = function () {
-	document.getElementById('messages').innerHTML = '';
-}
